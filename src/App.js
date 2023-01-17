8ab32cc95193929aac63baefd9b40d4030af971b
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-01-17 17:17:49 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import './App.css';
+import "./App.css";
 import News from "./components/news/News";
 import Heading from "./components/heading/Heading";
 import { useState, useEffect } from "react";
-import {Row} from "react-bootstrap";
+import { Row } from "react-bootstrap";
 
 function App() {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=f90e40c27eb24dc0bef89da59476d3cd`)
-    .then(res => res.json())
-    .then(data => setArticles(data.articles) );
+    fetch(
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=f90e40c27eb24dc0bef89da59476d3cd"
+    )
+      .then((res) => res.json())
+      .then((data) => setArticles(data.articles));
   }, []);
   return (
     <div className="App">
-    <Heading headingArticles={articles}></Heading>
-    <h3>Top News</h3>
-     <Row xs={1} md={2} className="g-4">
-    { 
-    articles.map((article,index) => <News article={article} key={index}></News>)
-    }
-     </Row>
+      <Heading headingArticles={articles}></Heading>
+      <h3>Top News: {articles.length}</h3>
+      <Row xs={1} md={2} className="g-4">
+        {articles.map((article, index) => (
+          <News article={article} key={index}></News>
+        ))}
+      </Row>
     </div>
   );
 }
