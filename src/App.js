6a53a4b802d522962b2aad643c240873e0ae9e58
fs-nirjhor <fsnirjhor2002@gmail.com>
2023-01-17 18:13:23 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
     fetch(
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=f90e40c27eb24dc0bef89da59476d3cd"
+      "https://gnews.io/api/v4/top-headlines?token=765ac2e88fa5ad490de1a20fbb046e96&topic=breaking-news"
     )
       .then((res) => res.json())
       .then((data) => setArticles(data.articles));
--- src/components/news/News.js
+++ src/components/news/News.js
@@ -3,19 +3,20 @@ import {Card,Col,Button} from "react-bootstrap";
 
 const News = (props) => {
   const {article} = props;
+  const {title,description,url,image} = article;
   const random = Math.round(Math.random()*10);
 return (
   <Col>
       <Card className="h-100">
-        <Card.Img variant="top" src={article.urlToImage} />
+        <Card.Img variant="top" src={image} />
         <Card.Body>
           <Card.Title>
-            {article.title}
+            {title}
           </Card.Title>
           <Card.Text>
-            {article.description}
+            {description}
           </Card.Text>
-          <Button href={article.url} variant="outline-primary">Show more</Button>
+          <Button href={url} variant="outline-primary">Show more</Button>
         </Card.Body>
         <Card.Footer>
           <small className="text-muted">Last Updated {random} minites ago</small>
